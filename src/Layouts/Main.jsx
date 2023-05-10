1ba07d44731d9821b13f8599381db7635ad0f576
arafataft <arafatcd@gmail.com>
2023-05-10 17:05:22 +0600

    footer nabbar

--- src/Layouts/Main.jsx
+++ src/Layouts/Main.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from 'react-router-dom';
+import Footer from '../Pages/Shared/Footer/Footer';
+import NavBar from '../Pages/Shared/Footer/NavBAr/NavBar';
 
 const Main = () => {
     return (
         <div>
+            <NavBar/>
             <Outlet/>
+            <Footer/>
         </div>
     );
 };
